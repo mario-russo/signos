c84778d785852ca747a253b2bdec1aeb1ce97f73
Mário Russo <mario.russo93@hotmail.com>
2023-07-29 11:52:29 -0300

    fix (caracteristica): correção das caracterisca do signo

--- utils/domain/caracteristicasDosSignos.ts
+++ utils/domain/caracteristicasDosSignos.ts
@@ -1,45 +1,83 @@
 import { signo } from "utils/types";
 
-export const signos:signo[] = [
+export const signos: signo[] = [
     {
         id: 1,
         nome: 'Aries',
         url: "~/assets/img/aries.jpg",
-        data:""
+        data: "",
+        altImage: '',
+        nomeImage: '',
+        caracteristicas: []
     },
     {
         id: 2,
         nome: 'Touro',
         url: "~/assets/img/touro.jpg",
-        data:""
+        data: "",
+        altImage: '',
+        nomeImage: '',
+        caracteristicas: []
     },
     {
         id: 3,
         nome: 'Gêmios',
         url: "~/assets/img/gemios.jpg",
-        data:""
+        data: "",
+        altImage: '',
+        nomeImage: '',
+        caracteristicas: []
     },
     {
         id: 4,
         nome: 'Câncer ',
         url: "~/assets/img/cancer.jpg",
-        data:""
+        data: "",
+        altImage: '',
+        nomeImage: '',
+        caracteristicas: []
     },
     {
         id: 5,
         nome: 'Leão ',
         url: "~/assets/img/leao.jpg",
-        data:""
+        data: "",
+        altImage: '',
+        nomeImage: '',
+        caracteristicas: []
     },
     {
         id: 6, nome: 'Virgem', url: "~/assets/img/virgem.jpg", data: "(23 de agosto a 22 de setembro):",
+        altImage: '',
+        nomeImage: '',
+        caracteristicas: []
     },
     {
-        id: 7, nome: 'Libra', url: "~/assets/img/libra.jpg", data: "(23 de setembro a 22 de outubro): "
+        id: 7, nome: 'Libra', url: "~/assets/img/libra.jpg", data: "(23 de setembro a 22 de outubro): ",
+        altImage: '',
+        nomeImage: '',
+        caracteristicas: []
     },
-    { id: 8, nome: 'Escorpião', url: "~/assets/img/escorpiao.jpg", data: "23 de outubro a 21 de novembro" },
-    { id: 9, nome: 'Sagitário', url: "~/assets/img/sagitario.jpg", data: "22 de novembro a 21 de dezembro" },
-    { id: 10, nome: 'Capricórnio', url: "~/assets/img/capricornio.jpg", data: "22 de dezembro a 19 de janeiro"},
-    { id: 11, nome: 'Aquário', url: "~/assets/img/aquario.jpg", data: "20 de janeiro a 18 de fevereiro" },
-    { id: 12, nome: 'Peixes', url: "~/assets/img/peixes.jpg", data: "19 de fevereiro a 20 de março" }
+    {
+        id: 8, nome: 'Escorpião', url: "~/assets/img/escorpiao.jpg", data: "23 de outubro a 21 de novembro",
+        altImage: '',
+        nomeImage: '',
+        caracteristicas: []
+    },
+    { id: 9, nome: 'Sagitário', url: "~/assets/img/sagitario.jpg", data: "22 de novembro a 21 de dezembro",
+    altImage:'',
+    nomeImage:'',
+    caracteristicas:[] },
+    { id: 10, nome: 'Capricórnio', url: "~/assets/img/capricornio.jpg", data: "22 de dezembro a 19 de janeiro",
+    altImage:'',
+    nomeImage:'',
+    caracteristicas:[] },
+    { id: 11, nome: 'Aquário', url: "~/assets/img/aquario.jpg", data: "20 de janeiro a 18 de fevereiro",
+    altImage:'',
+    nomeImage:'',
+    caracteristicas:[] },
+    { id: 12, nome: 'Peixes', url: "~/assets/img/peixes.jpg", data: "19 de fevereiro a 20 de março",
+    altImage:'',
+    nomeImage:'',
+    caracteristicas:[] }
 ]

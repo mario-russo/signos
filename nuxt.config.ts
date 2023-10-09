b3afd71292265d53f1dd5be3fc6dac5e68d560e7
Mário Russo <mario.russo93@hotmail.com>
2023-10-09 17:55:01 -0300

    Env [ variavel de ambiente ]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+
+    public: { baseUrl: process.env.API_URL || 'http://localhost:8080/', }
+  },
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',

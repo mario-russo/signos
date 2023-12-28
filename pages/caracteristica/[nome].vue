0f728f68000bfbd9b8e998234e180b75328c8da7
Mário Russo <mario.russo93@hotmail.com>
2023-12-28 14:26:12 -0300

    Refactor (css) : 3 componente

--- pages/caracteristica/[nome].vue
+++ pages/caracteristica/[nome].vue
@@ -1,140 +1,67 @@
 <template>
-    <div class="caixa  p-5">
-        <section class="pt-20">
+  <div class="p-5">
+    <section class="grid caixa">
+      <section class="anuncio shadow"><h1>anuncio</h1></section>
+      <div class="max-w-4xl mx-auto py-8 px-5 shadow conteudo">
+        <h2 class="text-xl font-semibold mb-4">
+          Caracteristicas do signo de {{ signo?.nomeImage }}
+        </h2>
+        <p>{{ signo?.data }}</p>
 
-            <section class="grid">
-            <div class="max-w-4xl mx-auto py-8">
-                <h1 class="text-xl font-semibold mb-4">Caracteristicas do signo de {{ signo?.nomeImage }}</h1>
-                <div class="titulo mt-10">
-                    <img class="rounded-full h-20" :src="signo?.url" :alt="signo?.altImage">
-                    <h1 class="text-3xl font-bold mb-4 mt-20">{{ signo?.nomeImage }}</h1>
-                </div>
-                <p> {{ signo?.data }}</p>
-
-                <p class="text-gray-700 leading-relaxed" v-for="caracteristica in signo?.caracteristicas">
-                   {{ caracteristica }}
-                 </p>
-            </div>
-            <div class="anuncio">
-                <h2>Seção de Anúncio</h2>
-                <p>
-                    Anúncio aqui.
-                </p>
-            </div>
-        </section>
-        </section>
-    </div>
+        <p
+          class="text-gray-700 leading-relaxed"
+          v-for="caracteristica in signo?.caracteristicas"
+        >
+          {{ caracteristica }}
+        </p>
+      </div>
+      <div class="anuncio shadow">
+        <h2>Seção de Anúncio</h2>
+        <p>Anúncio aqui.</p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
-    import{signoStore} from '~/stores/signoStore'
+import { signoStore } from "~/stores/signoStore";
 
-    const {nome} = useRoute().params
-    const id = parseInt(nome.toString())
-    const signo = signoStore().getBySignoId(id)
-    
-    onMounted(()=>{
+const { nome } = useRoute().params;
+const id = parseInt(nome.toString());
+const signo = signoStore().getBySignoId(id);
 
-    })
-
+onMounted(() => {});
 </script>
 
 <style scoped>
 @import url("~/assets/css/tamanhoFonte.css");
- .caixa{
-    min-height: 500px;
- }
- p {
-    font-size: 1.3rem;
-    /* Tamanho base da fonte */
-    line-height: 1.6;
-    /* Espaçamento entre linhas */
-    margin: 30px 0 10px 0;
-    text-align: justify;
-
+.caixa {
+  /* background-color: beige; */
+  display: flex;
+  flex-direction: row;
+  margin-top: 70px;
+  min-height: 80vh;
 }
-
-h1 {
-    font-size: 2.5rem;
-    margin-bottom: 1em;
-    /* Tamanho maior para títulos */
+.conteudo p {
+  font-size: 1.6rem;
 }
-
-h2 {
-    font-size: 2rem;
-    /* Tamanho para subtítulos */
-}
-
-.grid {
-    display: grid;
-    grid-template-columns: 5fr 1fr;
-    gap: 20px;
-    background-color: #fff;
-}
-
 .anuncio {
-    display: flex;
+  display: none;
+}
+@media (max-width: 800px) {
+  .caixa {
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 10px;
-    display: none;
+  }
+  .conteudo p {
+    font-size: 1.3rem;
+  }
+  .conteudo {
+    width: 100%;
+  }
 }
-
-.titulo {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: safe;
-    margin-bottom: 2rem;
-}
-
-.titulo h1 {
-    display: block;
-    margin-top: 35px;
-    margin-left: 20px;
-}
-.color{
-    background-color: #f7f7f7;
-    font-family: Helvetica;
-
-}
-
-/* Estilos para telas menores (até 600px) */
-@media (max-width: 600px) {
-    h1 {
-        font-size: 2.5rem;
-    }
-
-    h3 {
-        font-size: 1.9rem;
-    }
-
-    p {
-        font-size: 1.5rem;
-    }
-
-    .grid {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .titulo {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: safe;
-        margin-bottom: 1rem;
-    }
-}
-
-/* Estilos para telas médias (de 601px até 900px) */
-@media (min-width: 601px) and (max-width: 900px) {}
-
-/* Estilos para telas maiores (acima de 900px) */
-@media (min-width: 901px) {
-    body {
-        font-size: 18px;
-    }
+@media (max-width: 500px) {
+  .conteudo p {
+    font-size: 1.1rem;
+  }
 }
 </style>

4ed005627d8d16147c7877866a05cc6bb629e90a
Mário Russo <mario.russo93@hotmail.com>
2023-12-28 14:25:02 -0300

    Style ; remoção de comentario e espaços em branco

--- pages/caracteristica/index.vue
+++ pages/caracteristica/index.vue
@@ -1,24 +1,19 @@
 <template>
-    <div class="oi grid grid-cols-1 md:grid-cols-6">
-        <CardSignos :rota="redirectPath" class="col-start-1 col-end-6"></CardSignos>
-        <div class="justify-center">
-            <!-- <h1 class="pt-20 w-10">anuncio</h1> -->
-            <!-- {{ aries }}h2 -->
-        </div>
-    </div>
+  <div class="oi grid grid-cols-1 md:grid-cols-6">
+    <CardSignos :rota="redirectPath" class="col-start-1 col-end-6"></CardSignos>
+    <div class="justify-center"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { signoStore } from "~/stores/signoStore";
 
-import{signoStore}from '~/stores/signoStore'
+const storeSigno = signoStore();
 
+const aries = storeSigno.nome(11);
 
-const storeSigno= signoStore()
-
-const aries = storeSigno.nome(11)
-
-function redirectPath(signo:number){
-    useRouter().push({ path: "caracteristica/" + signo })
+function redirectPath(signo: number) {
+  useRouter().push({ path: "caracteristica/" + signo });
 }
 </script>
 
@@ -26,7 +21,7 @@ function redirectPath(signo:number){
 @import url("~/assets/css/tamanhoFonte.css");
 
 .oi {
-    min-height: 600px;
-    background-color: #f7f7f7;
+  min-height: 600px;
+  background-color: #f7f7f7;
 }
 </style>

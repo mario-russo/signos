4fb48fa87aa08b189e0ee1cf15a2108777ce36ef
Mário Russo <mario.russo93@hotmail.com>
2024-01-03 17:24:48 -0300

    Feat : adicionando google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
   runtimeConfig: {
-
-    public: { baseUrl: process.env.API_URL || 'http://localhost:8080/', }
+    public: {
+      baseUrl: process.env.API_URL || "http://localhost:8080/",
+      gtagId: process.env.GOOGLE_ANALYTICS_ID,
+    },
   },
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt', '@nuxt/image',],
-})
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@pinia/nuxt",
+    "@nuxt/image",
+    "@nuxtjs/google-analytics",
+  ],
+});
